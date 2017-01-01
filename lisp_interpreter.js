9ecaa85ee48d83abc7406b1666e72f695fe9b33a
Garima Kamboj <garima.kamboj@icloud.com>
2017-01-01 16:46:12 +0530

    Added comments to splitter function

--- lisp_interpreter.js
+++ lisp_interpreter.js
@@ -1,7 +1,8 @@
+// splitter function will first add the spaces around both the parenthesis
+// and then will split them on space thus separating each element in input
+//console.log(splitter('(+ 2 3)'))
 function splitter (input) {
   return input.replace(/\(/g, ' ( ')
               .replace(/\)/g, ' ) ')
               .split(' ')
 }
-
-console.log(splitter('(+ 2 3)'))
