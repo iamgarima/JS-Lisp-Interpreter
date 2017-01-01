ca214d02a48eab91db0dda1fe1f0ecf813c4d1b5
Garima Kamboj <garima.kamboj@icloud.com>
2017-01-01 16:57:39 +0530

    tokenize done

--- lisp_interpreter.js
+++ lisp_interpreter.js
@@ -1,8 +1,32 @@
-// splitter function will first add the spaces around both the parenthesis
-// and then will split them on space thus separating each element in input
-//console.log(splitter('(+ 2 3)'))
+// splitter function first add the spaces around both the parenthesis
+// and then split them on space thus separating each element in input
+// console.log(splitter('(+ 2 3)'))
 function splitter (input) {
   return input.replace(/\(/g, ' ( ')
               .replace(/\)/g, ' ) ')
               .split(' ')
+}
+
+
+// tokenize function removes the empty strings and the parenthesis elements
+// from the array and put each expression in a new array taking consideration
+// of the parenthesis
+// console.log(tokenize(splitter('(+ 2 3)'), []))
+function tokenize (input, list) {
+  var firstElem = input.shift()
+  if (firstElem === undefined) {
+    return list.pop();
+  }
+  if (firstElem === '') {
+    return tokenize(input, list)
+  }
+  if (firstElem === '(') {
+    list.push(tokenize(input, []))
+    return tokenize(input, list)
+  }
+  if (firstElem === ')') {
+    return list
+  }
+  list.push(firstElem)
+  return tokenize(input, list)
 }
